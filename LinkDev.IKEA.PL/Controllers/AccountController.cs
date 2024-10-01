5c68452c0abe8999e8aa7c642b71bddf7f8e330b
IsRaa Ahmed <IsRaa Ahmed@DESKTOP-GPAPH8D>
2024-10-01 13:26:41 +0300

    Part 03 Account Controller-SignUp

---
+++ LinkDev.IKEA.PL/Controllers/AccountController.cs
@@ -0,0 +1,70 @@
+﻿using LinkDev.IKEA.DAL.Entities.Identity;
+using LinkDev.IKEA.PL.ViewModels.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinkDev.IKEA.PL.Controllers
+{
+	public class AccountController : Controller
+	{
+		#region Services
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly SignInManager<ApplicationUser> _signInManager;
+		public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+		{
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
+		#endregion
+
+
+		#region Sign Up
+		[HttpGet] // Get: /Account/SignUp
+		public IActionResult SignUp()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> SignUp(SignUpViewModel model)
+		{
+			if (!ModelState.IsValid)
+				return BadRequest();
+			var user = await _userManager.FindByNameAsync(model.UserName);
+			if (user is { })
+			{
+				ModelState.AddModelError(nameof(SignUpViewModel.UserName), "This Username Is Already Used");
+				return View(model);
+			}
+			if (user is null)
+			{
+				user = new ApplicationUser()
+				{
+					FName = model.FirstName,
+					LName = model.LastName,
+					UserName = model.UserName,
+					Email = model.Email,
+					IsAgree = model.IsAgree,
+				};
+				var result = await _userManager.CreateAsync(user, model.Password);
+				if (result.Succeeded)
+					return RedirectToAction(nameof(SignIn));
+				foreach (var error in result.Errors)
+				{
+					ModelState.AddModelError(string.Empty, error.Description);
+				}
+			}
+
+			return View(model);
+		}
+		#endregion
+		#region SignIn
+		public IActionResult SignIn()
+		{
+			return View();
+		}
+		#endregion
+	}
+}
+
+
--- LinkDev.IKEA.PL/Program.cs
+++ LinkDev.IKEA.PL/Program.cs
@@ -1,9 +1,12 @@
 using LinkDev.IKEA.BLL.Services.Departments;
 using LinkDev.IKEA.BLL.Services.Employees;
+using LinkDev.IKEA.DAL.Entities.Identity;
 using LinkDev.IKEA.DAL.Persistence.Data;
 using LinkDev.IKEA.DAL.Persistence.Repositories.Departments;
 using LinkDev.IKEA.DAL.Persistence.Repositories.Employees;
 using LinkDev.IKEA.DAL.Persistence.Repositories.Employess;
+using LinkDev.IKEA.DAL.Persistence.UnitOfWork;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
@@ -26,22 +29,39 @@ namespace LinkDev.IKEA.PL
                 optionsBuilder.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
 
-            builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
-            builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+			//builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+			//builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+			builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-            builder.Services.AddScoped<IDepartmentService, DepartmentService>();
+			builder.Services.AddScoped<IDepartmentService, DepartmentService>();
             builder.Services.AddScoped<IEmployeeService,EmployeeServices>();
+			//builder.Services.AddTransient<IAttachmentService, AttachmentService>();
 
-            //builder.Services.AddScoped<ApplicationDbContext>();
-            //builder.Services.AddScoped<DbContextOptions<ApplicationDbContext>>((ServiceProvider) =>
-            //{
-            //    var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-            //    optionsBuilder.UseSqlServer("");
-            //    var options = optionsBuilder.Options;
-            //    return options;
-            //});
-            #endregion
-            var app = builder.Build();
+			builder.Services.AddIdentity<ApplicationUser, IdentityRole>((option) =>
+            {
+                option.Password.RequiredLength = 5;
+                option.Password.RequireDigit = true;
+                option.Password.RequireNonAlphanumeric = true; // *#%&
+                option.Password.RequireUppercase = true;
+                option.Password.RequireLowercase = true;
+                option.Password.RequiredUniqueChars = 1;
+                option.User.RequireUniqueEmail = true;
+                option.Lockout.AllowedForNewUsers = true;
+                option.Lockout.MaxFailedAccessAttempts = 5;
+                option.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            }).AddEntityFrameworkStores<ApplicationDbContext>();
+			//option.User.AllowedUserNameCharacters = "asmdma;asdms";
+
+			//builder.Services.AddScoped<ApplicationDbContext>();
+			//builder.Services.AddScoped<DbContextOptions<ApplicationDbContext>>((ServiceProvider) =>
+			//{
+			//    var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+			//    optionsBuilder.UseSqlServer("");
+			//    var options = optionsBuilder.Options;
+			//    return options;
+			//});
+			#endregion
+			var app = builder.Build();
 
             #region Configure Kestrel Middlewares
             // Configure the HTTP request pipeline.
